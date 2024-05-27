5c41fb09f7e61a4c63977b1560c5d37f594ac93f
FridahWatetu <fridahwatetumuthoni@gmail.com>
2024-05-27 09:21:21 +0300

    media settings

--- core/settings.py
+++ core/settings.py
@@ -167,8 +167,8 @@ SITE_ID = 3
 
 # statics files and images
 
-MEDIA_URL=os.path.join(BASE_DIR, 'images/')
-MEDIA_ROOT = os.path.join(BASE_DIR / 'images')
+MEDIA_URL=os.path.join('/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles" )# new
